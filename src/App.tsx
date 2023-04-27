2fa27e326f8f8c47e7cdf7cea9c4e75b5b8fd065
Jstdenis <jstdenis@outreach.com>
2023-04-26 23:39:22 -0400

    getting setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import AppThemeProvider from './Providers/AppThemeProvider'
+import { CharacterPage } from './Pages/Characters/CharacterPage'
+import { ApolloProvider } from '@apollo/client'
+import { WebClient } from './Utilities/WebClient'
 
 function App() {
 
   return (
-    <>
-    </>
+      <AppThemeProvider>
+    <BrowserRouter>
+        <ApolloProvider client={WebClient}>
+          <Routes>
+            <Route path={"/characters/*"} element={<CharacterPage />} />
+          </Routes>
+        </ApolloProvider>
+    </BrowserRouter>
+      </AppThemeProvider>
   )
 }
 
